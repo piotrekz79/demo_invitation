2a6d656f236f2071f88fdad40ae2b762e1c03e4f
piotrekz79 <piotrekz79@github.com>
2016-07-18 11:14:56 +0200

    calls changed as dbdump is invoked from main

--- mntn.sh
+++ mntn.sh
@@ -1,3 +1,2 @@
 #run TNO North topology
-cd /home/${USER}
-sudo mn --custom /home/${USER}/demo_invitation/coco_multidomain_invitation.py --topo tnonorth --controller remote,127.0.0.1
+sudo python coco_multidomain_invitation.py tn
--- mnts.sh
+++ mnts.sh
@@ -1,3 +1,2 @@
 #run TNO South topology
-cd /home/${USER}
-sudo mn --custom /home/${USER}/demo_invitation/coco_multidomain_invitation.py --topo tnosouth --controller remote,127.0.0.1
+sudo python coco_multidomain_invitation.py ts
